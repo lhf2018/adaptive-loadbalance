6ac1035cd65cc38a62785661d45fca2db466cac9
lhf2018 <1146953160@qq.com>
2019-10-26 21:19:53 +0800

    1:2:3轮询

--- workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
@@ -7,7 +7,6 @@ import org.apache.dubbo.rpc.RpcException;
 import org.apache.dubbo.rpc.cluster.LoadBalance;
 
 import java.util.List;
-import java.util.Random;
 
 /**
  * @author daofeng.xjf
@@ -18,28 +17,17 @@ import java.util.Random;
  * 选手需要基于此类实现自己的负载均衡算法
  */
 public class UserLoadBalance implements LoadBalance {
-    private static Random random=new Random();
-    private static int num=random.nextInt(3)+1;
+    private static int num=0;
     @Override
     public <T> Invoker<T> select(List<Invoker<T>> invokers, URL url, Invocation invocation) throws RpcException {
-        int temp=random.nextInt(3);
-        switch (num){
-            case 0:break;
-            case 1:temp=random.nextInt(2)+1;
-            break;
-            case 2:temp=random.nextInt(2)==0?0:2;
-            break;
-            case 3:temp=random.nextInt(2);
-            break;
-            case 4:temp=1;
-            break;
-            case 5:temp=0;
-            break;
+        num=(num+1)%6;
+        if(num==0){
+            return invokers.get(0);
+        }else if(num==1||num==2){
+            return invokers.get(1);
+        }else {
+            return invokers.get(2);
         }
-        num+=(temp+1);
-        num%=6;
-        System.out.println("------第 "+temp);
-        return invokers.get(temp);
     }
 //    @Override
 //    public <T> Invoker<T> select(List<Invoker<T>> invokers, URL url, Invocation invocation) throws RpcException {
